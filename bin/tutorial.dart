edc4acec37cf76d6f4db4d572946c7369a32f0fc
Kxben <kessbenantwi@gmail.com>
2024-09-04 19:39:18 +0000

    another touch

--- bin/tutorial.dart
+++ bin/tutorial.dart
@@ -1,8 +1,12 @@
 import 'dart:math';
 void main(List<String> arguments) {
+ //Introduction
   print('Hello world!');
   print(5+6);
+  //Excercise one
   print(sin(45*pi/180));
   print(1/sqrt(2));
-
+ bool yes = 10.isEven;
+  int kay = 3.3948930.round();
+print("$yes, you are not a 10 but a $kay");
 }
