362bc8d665c1aac1da5c18ed2ce115b4013c09e4
Kxben <kessbenantwi@gmail.com>
2024-09-05 22:35:33 +0000

    adding more

--- bin/tutorial.dart
+++ bin/tutorial.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 void main(List<String> arguments) {
- //Introduction
+/* //Introduction
   print('Hello world!');
   print(5+6);
   //Excercise one
@@ -9,4 +9,42 @@ void main(List<String> arguments) {
  bool yes = 10.isEven;
   int kay = 3.3948930.round();
 print("$yes, you are not a 10 but a $kay");
+int myAge=45;
+const testNumber = 88;
+const evenOdd = testNumber%5;
+var trial=evenOdd;
+trial +=5;
+print(evenOdd+trial);
+// Challenge 1
+int dogs=4;
+dogs++;
+print(dogs);
+int age=16;
+print(age);
+ age=30;
+print(age);*/
+const x=46;
+const y=10;
+const answer1=(x*100)+y;
+const answer2=(x*100)+(y*100);
+const answer3=(x*100)+(y/10);
+//print("$answer1, $answer2, $answer3");
+const a =2.0;
+const b =3.0; 
+const c =1.0;
+ 
+  //const a = 2.0;
+  //const b = 3.0;
+  //const c = 1.0;
+  final  root1= (-b+sqrt(b*b - 4*a*c))/(2*a);
+  final root2= (-b-sqrt(b*b - 4*a*c))/(2*a);
+  print("$root1 and $root2 are the roots of the expression");
+
+  //final root1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+  //final root2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+  print(root1);
+  print(root2);
+  const kwameIsangry=false && true ||true;
+  print(kwameIsangry);
+
 }
